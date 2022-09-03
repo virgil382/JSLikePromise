fde03f79c8b028d8f56a0b284df23d37fb540b26
virgil382 <virgil382@gmail.com>
2022-09-03 11:07:54 -0400

    Added .cpp and .js files that help to illustrate the limitations/differences of JSLike Promises vs. JavaScript Promises.

---
+++ Examples_JavaScript/differences.js
@@ -0,0 +1,17 @@
+
+//================================================================================
+async function difference01_function() {
+  // This JavaScript code is syntactically correct and legible.
+  return new Promise(
+    (resolve, reject) => {
+      resolve();
+    });
+}
+
+
+function difference01() {
+  let p = difference01_function();
+}
+
+difference01();
+
